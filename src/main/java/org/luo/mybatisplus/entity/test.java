02494315faa720b48c7107ffbdc5a3e9721abc8c
luoshuqiang <1246690778@qq.com>
2019-04-24 21:42:46 +0800

    first

---
+++ src/main/java/org/luo/mybatisplus/entity/test.java
@@ -0,0 +1,9 @@
+package org.luo.mybatisplus.entity;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.annotation.Resource;
+import java.security.Principal;
+
+public class test {
+}
