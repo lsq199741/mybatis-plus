389a19804d7ccf29570d7c0e22ae044580faf452
luoshuqiang <1246690778@qq.com>
2019-04-26 12:54:29 +0800

    添加代码生成器

--- src/main/java/org/luo/mybatisplus/MybatisPlusApplication.java
+++ src/main/java/org/luo/mybatisplus/MybatisPlusApplication.java
@@ -1,9 +1,11 @@
 package org.luo.mybatisplus;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("org.luo.mybatisplus.mapper")
 public class MybatisPlusApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/org/luo/mybatisplus/generator/Generator.java
@@ -0,0 +1,8 @@
+package org.luo.mybatisplus.generator;
+
+public class Generator {
+    public static void main(String[] args) {
+        CodeGenerator codeGenerator = new CodeGenerator();
+        codeGenerator.generator("user");
+    }
+}
