76c77ccc6359c442644576ccdfdab0c2f5fb36b2
luoshuqiang <1246690778@qq.com>
2019-04-30 17:25:26 +0800

    更新数据库文件

--- DB/springboot.sql
+++ DB/springboot.sql
@@ -11,11 +11,77 @@
  Target Server Version : 50722
  File Encoding         : 65001
 
- Date: 29/04/2019 10:26:01
+ Date: 30/04/2019 17:24:27
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for admin
+-- ----------------------------
+DROP TABLE IF EXISTS `admin`;
+CREATE TABLE `admin` (
+  `id` int(10) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `nickname` varchar(32) DEFAULT NULL COMMENT '昵称',
+  `username` varchar(32) DEFAULT NULL COMMENT '用户名',
+  `password` varchar(64) DEFAULT NULL COMMENT '密码',
+  `salt` varchar(20) DEFAULT NULL COMMENT '密码盐',
+  `create_admin` int(10) DEFAULT '0' COMMENT '创建管理员编号',
+  `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COMMENT='管理员用户表';
+
+-- ----------------------------
+-- Records of admin
+-- ----------------------------
+BEGIN;
+INSERT INTO `admin` VALUES (1, '骆书强', 'luoshuqiang', 'eeafb716f93fa090d7716749a6eefa72', '1', 0, '2019-04-30 13:23:17');
+INSERT INTO `admin` VALUES (2, '周凯旋', 'zhoukaixuan', 'eeafb716f93fa090d7716749a6eefa72', '1', 1, '2019-04-30 13:26:34');
+INSERT INTO `admin` VALUES (3, '张苗苗', 'zmm', 'eeafb716f93fa090d7716749a6eefa72', '1', 1, '2019-04-30 13:27:04');
+COMMIT;
+
+-- ----------------------------
+-- Table structure for admin_role
+-- ----------------------------
+DROP TABLE IF EXISTS `admin_role`;
+CREATE TABLE `admin_role` (
+  `admin_id` int(10) NOT NULL COMMENT '管理员编号',
+  `role_id` int(10) NOT NULL COMMENT '角色编号',
+  PRIMARY KEY (`admin_id`,`role_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='管理员权限表';
+
+-- ----------------------------
+-- Records of admin_role
+-- ----------------------------
+BEGIN;
+INSERT INTO `admin_role` VALUES (1, 1);
+INSERT INTO `admin_role` VALUES (2, 2);
+INSERT INTO `admin_role` VALUES (3, 3);
+COMMIT;
+
+-- ----------------------------
+-- Table structure for permission
+-- ----------------------------
+DROP TABLE IF EXISTS `permission`;
+CREATE TABLE `permission` (
+  `id` int(10) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `parent_id` int(10) DEFAULT NULL COMMENT '父id',
+  `name` varchar(32) DEFAULT NULL COMMENT '名称',
+  `type` varchar(32) DEFAULT NULL COMMENT '类型',
+  `permission` varchar(32) DEFAULT NULL COMMENT '许可名称',
+  `url` varchar(100) DEFAULT NULL COMMENT '接口url',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COMMENT='许可表';
+
+-- ----------------------------
+-- Records of permission
+-- ----------------------------
+BEGIN;
+INSERT INTO `permission` VALUES (1, NULL, '系统管理', 'menu', 'system', '');
+INSERT INTO `permission` VALUES (2, 1, '用户管理', 'menu', 'systemUserList', '/admin/UserList');
+INSERT INTO `permission` VALUES (3, 1, '角色管理', 'menu', 'systemRoleList', '/admin/RoleList');
+COMMIT;
 
 -- ----------------------------
 -- Table structure for role
@@ -24,7 +90,7 @@ DROP TABLE IF EXISTS `role`;
 CREATE TABLE `role` (
   `id` int(10) NOT NULL AUTO_INCREMENT COMMENT '角色编号',
   `name` varchar(32) NOT NULL COMMENT '角色名称',
-  `ctime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `title` varchar(32) NOT NULL COMMENT '角色名称',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COMMENT='角色表';
 
@@ -32,9 +98,30 @@ CREATE TABLE `role` (
 -- Records of role
 -- ----------------------------
 BEGIN;
-INSERT INTO `role` VALUES (1, '系统管理员', '2019-04-28 18:23:00');
-INSERT INTO `role` VALUES (2, '普通管理员', '2019-04-28 18:23:40');
-INSERT INTO `role` VALUES (3, '后台管理员', '2019-04-28 18:23:40');
+INSERT INTO `role` VALUES (1, 'sys_admin', '系统管理员');
+INSERT INTO `role` VALUES (2, 'admin', '普通管理员');
+INSERT INTO `role` VALUES (3, 'back_admin', '后台管理员');
+COMMIT;
+
+-- ----------------------------
+-- Table structure for role_permission
+-- ----------------------------
+DROP TABLE IF EXISTS `role_permission`;
+CREATE TABLE `role_permission` (
+  `role_id` int(10) NOT NULL COMMENT '角色id',
+  `permission_id` int(10) NOT NULL COMMENT '许可id',
+  PRIMARY KEY (`role_id`,`permission_id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='角色许可表';
+
+-- ----------------------------
+-- Records of role_permission
+-- ----------------------------
+BEGIN;
+INSERT INTO `role_permission` VALUES (1, 1);
+INSERT INTO `role_permission` VALUES (1, 2);
+INSERT INTO `role_permission` VALUES (1, 3);
+INSERT INTO `role_permission` VALUES (2, 2);
+INSERT INTO `role_permission` VALUES (2, 3);
 COMMIT;
 
 -- ----------------------------
