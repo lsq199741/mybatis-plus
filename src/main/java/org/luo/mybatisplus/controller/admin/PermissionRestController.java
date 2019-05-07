6b809dfc4e623d5e66056e903e715b133b3fd9ae
luoshuqiang <1246690778@qq.com>
2019-05-07 18:37:45 +0800

    新建管理员角色管理模块，管理员许可管理模块

--- src/main/java/org/luo/mybatisplus/controller/admin/PermissionRestController.java
+++ src/main/java/org/luo/mybatisplus/controller/admin/PermissionRestController.java
@@ -1,4 +1,27 @@
 package org.luo.mybatisplus.controller.admin;
 
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.luo.mybatisplus.service.PermissionService;
+import org.luo.mybatisplus.service.RolePermissionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@Api(value = "/adminLogin", tags = "管理员许可管理模块")
+@Slf4j
+@Validated
+@RestController
+@RequestMapping("/adminPermission")
 public class PermissionRestController {
+
+    @Autowired
+    private PermissionService permissionService;
+
+    @Autowired
+    private RolePermissionService rolePermissionService;
+
+
 }
--- src/main/java/org/luo/mybatisplus/controller/admin/RoleRestController.java
+++ src/main/java/org/luo/mybatisplus/controller/admin/RoleRestController.java
@@ -1,4 +1,27 @@
 package org.luo.mybatisplus.controller.admin;
 
+
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.luo.mybatisplus.service.AdminRoleService;
+import org.luo.mybatisplus.service.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(value = "/adminLogin", tags = "管理员角色管理模块")
+@Slf4j
+@Validated
+@RestController
+@RequestMapping("/adminRole")
 public class RoleRestController {
+
+    @Autowired
+    private RoleService roleService;
+
+    @Autowired
+    private AdminRoleService adminRoleService;
+
+
 }
